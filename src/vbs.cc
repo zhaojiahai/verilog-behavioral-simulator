22c6bc5b5cc5593e08a6df7d38bdcb6f3abdbb8a
Jimen Ching <jimen.ching@gmail.com>
2011-08-02 11:42:16 -1000

    Start support for external applications.

---
+++ src/vbs.cc
@@ -0,0 +1,11 @@
+// Verilog Behavioral Simulator
+// Copyright (C) 2011 Lay Hoon Tho, Jimen Ching
+//
+// This file is part of the Verilog Behavioral Simulator package.
+// See the file COPYRIGHT for copyright and disclaimer information.
+// See the file COPYING for the licensing terms and conditions.
+// See the file CONTRIBUTORS for a list of contributing authors.
+//
+// vbs.cc
+
+#include "vbs.h"
